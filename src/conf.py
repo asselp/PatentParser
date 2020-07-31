4e48850aff7ea584edfb00238a1b1362b9ba0ca4
asselp <isassell2@gmail.com>
2020-08-01 04:53:06 +0500

    Initial commit

--- src/conf.py
+++ src/conf.py
@@ -1,16 +1,31 @@
-
 KP_MAIN = 'https://copyright.kazpatent.kz/'
 HEADERS = {
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/83.0.4103.116 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,'
-                  '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-User': '?1',
-        'Sec-Fetch-Dest': 'document',
-        'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
-    }
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/83.0.4103.116 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,'
+              '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+}
+SCROLL_HEADERS = {
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/83.0.4103.116 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': '*/*',
+    'Origin': 'https://copyright.kazpatent.kz',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://copyright.kazpatent.kz/',
+    'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+}
+PARAMS = {'$$ajaxid': ''}
+KP_AJAX = 'https://copyright.kazpatent.kz/xpd/xpd.nsf/xpd.xsp'
--- src/crawler.py
+++ src/crawler.py
@@ -1,53 +1,83 @@
-from typing import List
+from typing import List, Optional
 
 import requests
 from lxml.html import fromstring
 
-from src.conf import HEADERS, KP_MAIN
+from src.utils import get_logger
+from src.conf import HEADERS, KP_MAIN, SCROLL_HEADERS, PARAMS, KP_AJAX
 from src.models import Patent
-
-r = 'aidana'
-q = 34
 
 
 class KazPatentCrawler:
 
-    def __init__(self):
+    def __init__(self, quantity):
         self.headers = HEADERS
+        self.scroll_header = SCROLL_HEADERS
+        self.params = PARAMS
         self.view_id = None
         self.session_id = None
+        self.data = None
+        self.quantity = quantity
+        self.logger = get_logger('KazPatentCrawler')
 
     def _prepare(self, response) -> None:
+        """
+            Prepare crawler to make a post request
+        """
         tree = fromstring(response.text)
         self.view_id: str = tree.xpath('.//input[@id="view:_id1__VUID"]/@value')[0]
-        self.session_id = response.cookies.get('SessionID')
+        self.session_id = dict(SessionID=dict(response.cookies).get("SessionID"))
+        self.data = f"%24%24viewid={self.view_id}&%24%24xspsubmitid=QueryLazyLoadHandlerRA_server&%24%24xspexecid=' \
+                    '&%24%24xspsubmitvalue={self.quantity - 35}&%24%24xspsubmitscroll=0%7C0&view%3A_id1=view%3A_id1"
+        self.logger.info('Parsing started')
 
-    def _get_preloaded_page(self) -> bytes:
+    def _get_preloaded_page(self) -> Optional[bytes]:
         """
             1. Get session_id and view_id
             2. Return the preloaded table with 35 patents
         """
-        response = requests.get(
-            url=KP_MAIN,
-            headers=self.headers,
-            timeout=20
-        )
-        # Todo: handle request exceptions
-        self._prepare(response)
-        return response.content
+        try:
+            response = requests.get(
+                url=KP_MAIN,
+                headers=self.headers,
+                timeout=20
+            )
+            self._prepare(response)
+            return response.content
+        except requests.exceptions.Timeout as e:
+            self.logger.error(f'Request exception: {e}')
+            return
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f'Request exception: {e}')
+            return
 
-    def _scroll(self) -> List[Patent]:
-        # self.view_id
-        # self.session_id
-        return []
+    def _get_ajax_response(self) -> Optional[bytes]:
+        """
+            Return the AJAX response
+        """
+        try:
+            response = requests.post(
+                timeout=20,
+                url=KP_AJAX,
+                headers=SCROLL_HEADERS,
+                params=self.params,
+                cookies=self.session_id,
+                data=self.data
+            )
+            return response.content
+        except requests.exceptions.Timeout as e:
+            self.logger.error(f'Request exception: {e}')
+            return
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f'Request exception: {e}')
+            return
 
-    def _get_first_patents(self) -> List[Patent]:
+    def _parse_patents(self, page) -> List[Patent]:
         _patents = []
-        preloaded_page: bytes = self._get_preloaded_page()
-        tree = fromstring(preloaded_page)
-        rows: List = tree.xpath('//table[@id="gridDataTablet"]//tr[@class="itsFeedHover"]')
+        tree = fromstring(page.decode('utf-8'))
+        rows = tree.xpath('//table//tr[@class="itsFeedHover"]')
         for row in rows:
-            td: list = row.xpath('./td//text()')
+            td: List = row.xpath('./td//text()')
             _patents.append(
                 Patent(
                     id=td[0],
@@ -62,19 +92,44 @@ class KazPatentCrawler:
             )
         return _patents
 
-    def get_patents(self, count: int = 200):
+    def _get_first_patents(self) -> List[Patent]:
+        _patents = []
+        retries = 3
+        while retries:
+            retries -= 1
+            preloaded_page: bytes = self._get_preloaded_page()
+            self.logger.info('Got preloaded page')
+            if not preloaded_page:
+                continue
+            return self._parse_patents(preloaded_page)
+
+    def _get_ajax_patents(self) -> List[Patent]:
+        _patents = []
+        retries = 3
+        while retries:
+            retries -= 1
+            ajax_page: bytes = self._get_ajax_response()
+            self.logger.info('Got ajax response')
+            if not ajax_page:
+                continue
+            return self._parse_patents(ajax_page)
+
+    def get_patents(self) -> List[Patent]:
         _patents: List[Patent] = self._get_first_patents()
-        """Get the remaining patents by scrolling"""
-        finished = False
-        while not finished:
-            patents_batch: List[Patent] = self._scroll()
-            _patents.extend(patents_batch)
-            finished = True
-        return _patents
+        if _patents:
+            self.logger.info('Preloaded patents parsed')
+            finished = False
+            while not finished:
+                patents_batch: List[Patent] = self._get_ajax_patents()
+                if patents_batch:
+                    self.logger.info('AJAX page patents parsed')
+                    _patents.extend(patents_batch)
+                    finished = True
+                self.logger.info('Parsing is over')
+                return _patents
 
 
 if __name__ == '__main__':
-    crawler = KazPatentCrawler()
-    patents = crawler.get_patents()
-    for patent in patents:
-        print(patent.dict())
+    crawler = KazPatentCrawler(40)
+
+    print(crawler.get_patents())
